19ca42fb85f94cd7ecd741d02640ccd1522fe8c3
Virsaviya <14098014+virsaviya@users.noreply.github.com>
2025-03-18 18:49:01 -0700

    Submit review and persist (in server memory)

--- src/components/StarRatingInput.tsx
+++ src/components/StarRatingInput.tsx
@@ -1,13 +1,52 @@
 import React from 'react';
 
+import Review from '@/data/Review';
 import StarRating from './StarRating';
 
 const StarRatingInput = () => {
   const [rating, setRating] = React.useState<number>(0);
   const [hoverRating, setHoverRating] = React.useState<number>(0);
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = e.currentTarget.elements;
+    const newReview: Omit<Review, 'id'> = {
+      rating: Number((formData.namedItem('rating') as HTMLInputElement)?.value),
+      author: (formData.namedItem('author') as HTMLInputElement)?.value,
+      review: (formData.namedItem('review') as HTMLInputElement)?.value || null,
+    };
+
+    postReview(newReview);
+  };
+
+  const postReview = async (review: Omit<Review, 'id'>) => {
+    try {
+      const resp = await fetch('http://localhost:3000/api/review', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(review),
+      });
+
+      if (!resp.ok)
+        throw new Error(
+          `An error occurred submitting the review: Status ${resp.status}`,
+        );
+
+      const submittedReview = await resp.json();
+      console.log('success!', submittedReview);
+    } catch (err) {
+      throw new Error(
+        'An error occurred submitting the review, please try again.',
+      );
+    }
+  };
+
   return (
-    <section className="flex flex-col items-center">
+    <form
+      onSubmit={handleSubmit}
+      method="post"
+      className="flex flex-col items-center"
+    >
       <StarRating
         rating={rating}
         isInput={true}
@@ -16,12 +55,16 @@ const StarRatingInput = () => {
         hoverRating={hoverRating}
         setHoverRating={setHoverRating}
       />
+      <input name="rating" type="hidden" value={rating} />
+      <input name="author" type="text" placeholder="author" />
+      <textarea name="review" placeholder="Write a review..." />
       <input
         type="submit"
-        className="mt-10 h-10 px-6 font-semibold rounded-md bg-black text-white"
+        className="mt-10 h-10 px-6 font-semibold rounded-md text-white"
         value="Submit review"
+        disabled={rating === 0}
       />
-    </section>
+    </form>
   );
 };
 
--- src/pages/api/review.ts
+++ src/pages/api/review.ts
@@ -1,16 +1,37 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import Review from "@/data/Review";
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import reviews from '@/data/reviewsData';
+import Review from '@/data/Review';
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Review[]>
+  res: NextApiResponse<Review | { error: string }>,
 ) {
-  if (req.method === "GET") {
+  if (req.method === 'GET') {
     // fetch single review by id
     res.status(200);
-  } else if (req.method === "POST") {
-    // "save" single review by id
-    res.status(200);
+  } else if (req.method === 'POST') {
+    try {
+      const { rating, author, review = null } = req.body;
+
+      if (!rating || typeof rating !== 'number' || rating < 1 || rating > 5)
+        return res.status(400).json({ error: 'Invalid rating (must be 1-5)' });
+      if (!author || typeof author !== 'string')
+        return res
+          .status(400)
+          .json({ error: 'Invalid Author (must be a string)' });
+
+      const newReview: Review = {
+        id: reviews.length + 1,
+        rating,
+        author,
+        review: typeof review !== 'string' ? null : review,
+      };
+      reviews.push(newReview);
+      res.status(201).json(newReview);
+    } catch (err) {
+      res.status(500).json({ error: 'Failed to save review' });
+    }
   } else {
     res.status(404);
   }
